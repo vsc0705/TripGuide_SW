0b25ac43cd3a49e68bd5bf8c05cec7e76d35a4a1
kimbeopseong <kbs54963@hanmail.net>
2020-05-28 00:24:11 +0900

    profile login

--- app/src/main/java/com/example/trip2/ui/set/SetFragment.java
+++ app/src/main/java/com/example/trip2/ui/set/SetFragment.java
@@ -1,9 +1,12 @@
 package com.example.trip2.ui.set;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -12,14 +15,64 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import com.example.trip2.MainActivity;
 import com.example.trip2.R;
 
 public class SetFragment extends Fragment {
+    TextView textView_startdate, textView_enddate;
+    Button btn_start, btn_end;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         View view=inflater.inflate(R.layout.fragment_set, container, false);
+        textView_startdate=view.findViewById(R.id.textView_startdate);
+        textView_enddate=view.findViewById(R.id.textView_enddate);
+        btn_start=view.findViewById(R.id.btn_start);
+        btn_end=view.findViewById(R.id.btn_end);
+
+        btn_start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showStartDate();
+            }
+        });
+        btn_end.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showEndDate();
+            }
+        });
+
+
         return view;
+    }
+
+
+    void showStartDate() {
+        DatePickerDialog datePickerDialog = new DatePickerDialog(getActivity(), new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+
+                textView_startdate.setText(year+"."+(month+1)+"."+dayOfMonth);
+
+            }
+        },2020, 5, 28);
+
+        datePickerDialog.setMessage("메시지");
+        datePickerDialog.show();
+    }
+    void showEndDate() {
+        DatePickerDialog datePickerDialog = new DatePickerDialog(getActivity(), new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+
+                textView_enddate.setText(year+"."+(month+1)+"."+dayOfMonth);
+
+            }
+        },2020, 5, 28);
+
+        datePickerDialog.setMessage("메시지");
+        datePickerDialog.show();
     }
 
 }
