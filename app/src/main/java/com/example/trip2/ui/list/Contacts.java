00e2fe6f756c1551e1b267763783f995d705eeab
kimbeopseong <kbs54963@hanmail.net>
2020-05-29 22:00:40 +0900

    message 기능 수정

--- app/src/main/java/com/example/trip2/ui/list/Contacts.java
+++ app/src/main/java/com/example/trip2/ui/list/Contacts.java
@@ -3,11 +3,12 @@ package com.example.trip2.ui.list;
 public class Contacts {
 
     public Contacts(){}
-    public Contacts(String name) {
+    public Contacts(String name, String status) {
         this.name = name;
+        this.status = status;
     }
 
-    public String name;
+    public String name, status;
 
     public String getName() {
         return name;
@@ -15,6 +16,14 @@ public class Contacts {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
     }
 
 }
