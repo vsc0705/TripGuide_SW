5976069ad91046db4ffef1c4490a7642e81a5d56
jinjin <oh9610@naver.com>
2020-06-03 02:05:48 +0900

    feed_write_commit..(0603 0205)

--- app/src/main/java/com/example/trip2/Thirdctivity.java
+++ app/src/main/java/com/example/trip2/Thirdctivity.java
@@ -1,14 +1,99 @@
 package com.example.trip2;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.media.Image;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+
+import com.example.trip2.ui.home.FeedWriteFragment;
+
+import java.io.ByteArrayOutputStream;
 
 public class Thirdctivity extends AppCompatActivity {
+    //여기
+
+    private final int GET_GALLERY_IMAGE = 200;
+    private ImageView imageview;
+    ImageButton btn_change;
+    ImageButton btn_ok;
+
+    byte[] byteArray;
+    Bitmap bitmap;
+    ByteArrayOutputStream stream;
+
+
+    //여기
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_thirdctivity);
+
+        //여기
+        imageview = (ImageView) findViewById(R.id.image);
+
+
+
+        btn_change = (ImageButton) findViewById(R.id.btn_change);
+        btn_change.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent. setDataAndType(android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI, "image/*");
+                startActivityForResult(intent, GET_GALLERY_IMAGE);
+            }
+        });
+
+
+        btn_ok = (ImageButton) findViewById(R.id.btn_ok);
+        btn_ok.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                bitmap = ((BitmapDrawable)imageview.getDrawable()).getBitmap();
+                FeedWriteFragment fragment = new FeedWriteFragment();
+
+
+
+                Bundle bundle = new Bundle();
+                bundle.putParcelable("image", bitmap);
+                bundle.putString("test","@@@");
+                fragment.setArguments(bundle);
+
+
+                finish();
+
+
+            }
+        });
+        //여기
+
+
     }
+
+    //여기
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == GET_GALLERY_IMAGE && resultCode == RESULT_OK && data != null && data.getData() != null) {
+
+            Uri selectedImageUri = data.getData();
+            imageview.setImageURI(selectedImageUri);
+        }
+    }
+
+    //여기
 }
--- app/src/main/java/com/example/trip2/ui/home/FeedWriteFragment.java
+++ app/src/main/java/com/example/trip2/ui/home/FeedWriteFragment.java
@@ -1,8 +1,11 @@
 package com.example.trip2.ui.home;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,14 +17,19 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.trip2.R;
+import com.example.trip2.Thirdctivity;
+
+import java.io.ByteArrayOutputStream;
 
 public class FeedWriteFragment extends Fragment {
-    private static final int RESULT_OK = 0;
     //여기
     Button change;
-    private final int GET_GALLERY_IMAGE = 200;
     ImageView imageview;
 
+    Bitmap bitmap;
+    Bundle bundle;
+
+    String test;
 
     //여기
 
@@ -30,17 +38,21 @@ public class FeedWriteFragment extends Fragment {
         View view=inflater.inflate(R.layout.fragment_feedwrite, container, false);
         //여기
 
+
+        imageview = (ImageView) view.findViewById(R.id.feed_veiw);
+
+
+
         change = (Button) view.findViewById(R.id.btn_change_photo);
         change.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(Intent.ACTION_PICK);
-                intent. setDataAndType(android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI, "image/*");
-                startActivityForResult(intent, GET_GALLERY_IMAGE);
+                Intent intent = new Intent(getActivity(), Thirdctivity.class);
+                startActivity(intent);
+
             }
         });
 
-        imageview = (ImageView) view.findViewById(R.id.feed_veiw);
 
 
         //여기
@@ -49,16 +61,6 @@ public class FeedWriteFragment extends Fragment {
         return view;
     }
     //여기
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        if (requestCode == GET_GALLERY_IMAGE && resultCode == RESULT_OK && data != null && data.getData() != null) {
-
-            Uri selectedImageUri = data.getData();
-            imageview.setImageURI(selectedImageUri);
-        }
-    }
 
 
 
