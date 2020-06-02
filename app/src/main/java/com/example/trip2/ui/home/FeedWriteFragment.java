1114199dad042e25ffdbd9501412d373456a8c0e
jinjin <oh9610@naver.com>
2020-06-03 00:16:32 +0900

    feed_write_commit..(0603 1216)

--- app/src/main/java/com/example/trip2/ui/home/FeedWriteFragment.java
+++ app/src/main/java/com/example/trip2/ui/home/FeedWriteFragment.java
@@ -1,20 +1,67 @@
 package com.example.trip2.ui.home;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.trip2.R;
 
 public class FeedWriteFragment extends Fragment {
+    private static final int RESULT_OK = 0;
+    //여기
+    Button change;
+    private final int GET_GALLERY_IMAGE = 200;
+    ImageView imageview;
+
+
+    //여기
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         View view=inflater.inflate(R.layout.fragment_feedwrite, container, false);
+        //여기
+
+        change = (Button) view.findViewById(R.id.btn_change_photo);
+        change.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent. setDataAndType(android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI, "image/*");
+                startActivityForResult(intent, GET_GALLERY_IMAGE);
+            }
+        });
+
+        imageview = (ImageView) view.findViewById(R.id.feed_veiw);
+
+
+        //여기
+
+
         return view;
     }
+    //여기
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == GET_GALLERY_IMAGE && resultCode == RESULT_OK && data != null && data.getData() != null) {
+
+            Uri selectedImageUri = data.getData();
+            imageview.setImageURI(selectedImageUri);
+        }
+    }
+
+
+
+    //여기
+
 }
