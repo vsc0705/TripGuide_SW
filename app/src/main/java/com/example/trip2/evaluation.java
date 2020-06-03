01b70edd1a6079da4a69fcaca4e93ec32da79c79
KimGyeongHeon <kyasd658@naver.com>
2020-06-03 16:15:49 +0900

    평가 레이아웃만 완성 자바는 아직 readme 경헌에 평가 추가

---
+++ app/src/main/java/com/example/trip2/evaluation.java
@@ -0,0 +1,14 @@
+package com.example.trip2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class evaluation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_evaluation);
+    }
+}
