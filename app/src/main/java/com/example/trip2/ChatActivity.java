cbb94eef86483e70b3520450b13bd5bcd8cc2ee5
105pm.dev <105pm.dev@gmail.com>
2020-06-03 16:49:51 +0900

    Change ChatRoom DB structure and fix some bug

--- app/src/main/java/com/example/trip2/ChatActivity.java
+++ app/src/main/java/com/example/trip2/ChatActivity.java
@@ -202,7 +202,7 @@ public class ChatActivity extends AppCompatActivity {
                                     userMessagesList.smoothScrollToPosition(userMessagesList.getAdapter().getItemCount());
                                     break;
                                 case MODIFIED:
-                                    // 여기때문에 채팅 증발현상 생김
+                                    // 여기때문에 채팅 증발현상 생길 가능성 존재
                                     messagesList.remove(messagesList.size()-1);
                                     messagesList.add(messages);
                                     messageAdapter.notifyDataSetChanged();
